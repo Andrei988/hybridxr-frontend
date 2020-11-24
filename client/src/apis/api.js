3f32d21870e750c61b340c7f7b670f8b10c46cfb
andrei <blalanuta@inbox.ru>
2020-11-24 11:00:30 +0100

    configured sll

--- client/src/apis/api.js
+++ client/src/apis/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export const dev_url_clean = "http://127.0.0.1:8000/";
-export const dev_url = "http://127.0.0.1:8000/api/";
-export const prod_url = "http://snurfer98.pythonanywhere.com/api/";
-export const prod_url_clean = "http://snurfer98.pythonanywhere.com/";
+export const dev_url_clean = "https://127.0.0.1:8000/";
+export const dev_url = "https://127.0.0.1:8000/api/";
+export const prod_url = "https://snurfer98.pythonanywhere.com/api/";
+export const prod_url_clean = "https://snurfer98.pythonanywhere.com/";
 
 export default axios.create({ 
   baseURL: prod_url,
